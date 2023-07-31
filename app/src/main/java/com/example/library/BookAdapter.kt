995ae525cc290e913f89746d6860149903554788
Anastasia <stepanovawm@gmail.com>
2023-07-31 21:42:29 +0300

    Created xmls, raw RV adapter

---
+++ app/src/main/java/com/example/library/BookAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.library
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class BookAdapter(private val dataSet: Array<String>) :
+    RecyclerView.Adapter<BookAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+
+        init {
+            textView = view.findViewById(R.id.view_holder)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.view_holder, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = dataSet.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = dataSet[position]
+    }
+}
